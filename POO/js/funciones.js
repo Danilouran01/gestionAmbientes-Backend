61b5368e6e317f8305530afe61cba1bafd52cf41
unknown <SENA@ITAAPRCSRFSD406.FormacionSena.RED>
2023-02-14 13:39:29 -0500

    Ver ambientes prestados

---
+++ POO/js/funciones.js
@@ -0,0 +1,12 @@
+
+function añadirObservaciones(id){
+    let observacion = window.prompt("Ingrese sus observaciones");
+    window.location.href = window.location.href + "?observacion=" + observacion + "&id=" + id ;
+}
+
+function observacionAñadida() {
+    alert('observacion añadida con exito');
+    
+}
+
+
