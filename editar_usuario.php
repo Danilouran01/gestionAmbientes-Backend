91d7cd4fdb125bbbe66e384c30793030d7717f2b
unknown <SENA@ITAAPRREAFSP518.FormacionSena.RED>
2023-02-22 11:45:40 -0500

    de html a php

--- editar_usuario.php
+++ editar_usuario.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../Css/editar_usuario.css">
+    <link rel="stylesheet" type="text/css" href="./Css/editar_usuario.css">
     <script src="https://kit.fontawesome.com/503089e863.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../Css/global.css">
+    <link rel="stylesheet" href="./Css/global.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200&display=swap" rel="stylesheet">
@@ -17,11 +17,11 @@
 <body>
 
     <div class="parte-superior">
-        <img class="logo" src="../images/logo sena.png" alt="">
+        <img class="logo" src="./images/logo sena.png" alt="">
         <h1 class="titulo">Gestión de Ambientes</h1>
         <div class="dropdown">
             <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <img class="perfil" src="../images/Boton Administrador.png" alt="">
+                <img class="perfil" src="./images/Boton Administrador.png" alt="">
             </button>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#" class="btn-b btns-b" data-bs-toggle="modal" data-bs-target="#editar_perfil">Editar perfil</a></li>
@@ -37,8 +37,8 @@
 
     <div class="flex">
         <div class="botones-principales">
-            <a href="./prestamo_ambientes.html" class="btn-1" >Prestamo de ambientes</a>
-            <a href="./prestamo_dispositivos.html" class="btn-1 btn-0">Prestamo de dispositivos</a>
+            <a href="./registrarPrestamoAmbiente.php" class="btn-1" >Prestamo de ambientes</a>
+            <a href="./prestamo_dispositivos.php" class="btn-1 btn-0">Prestamo de dispositivos</a>
         </div>
         <div class="herencia">
             <div class="buscador">
